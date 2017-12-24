907ee4a0002b2a41f62f73f77f7b113bf1776e19
caasi Huang <caasi.igd@gmail.com>
2017-12-24 16:38:10 +0800

    Implement a part of the chapter 10

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,29 @@
 module Main where
 
-import Graphics.SOE
+import Graphics.SOE hiding (Region)
 import Lib
---import Shape
-import SimpleGraphics
-import Draw
-import Perimeter
+import Picture
 
-sh1 = Rectangle 3 2
-sh2 = Ellipse 1 1.5
-sh3 = RtTriangle 3 2
-sh4 = Polygon [(-2.5, 2.5), (-1.5, 2.0), (-1.1, 0.2), (-1.7, -1.0), (-3.0, 0)]
+-- 不定義奇怪的 infix oprator 是怕讀者還不習慣嗎？
+xUnion :: Region -> Region -> Region
+p1 `xUnion` p2 = (p1 `Intersect` Complement p2) `Union` (p2 `Intersect` Complement p1)
 
-type ColoredShapes = [(Color, Shape)]
-shs :: ColoredShapes
-shs = [(Red, sh1), (Blue, sh2), (Yellow, sh3), (Magenta, sh4)]
+r1 = Shape (Rectangle 3 2)
+r2 = Shape (Ellipse 1 1.5)
+r3 = Shape (RtTriangle 3 2)
+r4 = Shape (Polygon [(-2.5, 2.5), (-3.0, 0), (-1.7, -1.0), (-1.1, 0.2), (-1.5, 2.0)])
 
-drawShapes :: Window -> ColoredShapes -> IO ()
-drawShapes w css
-  = sequence_ (map aux css)
-    where aux (c, s) = drawInWindow w (withColor c (shapeToGraphic s))
+reg1 = r3 `xUnion` (r1 `Intersect` Complement r2 `Union` r4)
+pic1 = Region Blue reg1
 
-conCircles = map circle [2.4, 2.1 .. 0.3]
+reg2 = let circle = Shape (Ellipse 0.5 0.5)
+           square = Shape (Rectangle 1 1)
+       in (Scale (2, 2) circle)
+          `Union` (Translate (1, 0) square)
+          `Union` (Translate (-1, 0) square)
+pic2 = Region Yellow (Translate (0, -1) reg2)
 
-coloredCircles
-  = zip [Black, Blue, Green, Cyan, Red, Magenta, Yellow, White]
-        conCircles
+pic3 = pic2 `Over` pic1
 
 main :: IO ()
-main = runGraphics (
-  do w <- openWindow "Drawing Shapes" (xWin, yWin)
-     drawShapes w coloredCircles
-     putStrLn . show $ perimeter sh2
-     spaceClose w
-  )
+main = draw "My Pictures" pic3
